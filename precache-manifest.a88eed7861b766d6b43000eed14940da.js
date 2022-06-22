63c516dd00779388879652048bc763deca79bd44
guoz5658 <guoz5658@outlook.com>
2022-06-22 21:20:32 +0800

    [publish] gh-pages

---
+++ precache-manifest.a88eed7861b766d6b43000eed14940da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0b9f51b69d519479a740c18881f7c5",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "fe037e9493fc06d42060",
+    "url": "/resume/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fd1a25ae27544c207377",
+    "url": "/resume/static/js/2.fa09f7e7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/resume/static/js/2.fa09f7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe037e9493fc06d42060",
+    "url": "/resume/static/js/main.3c77b873.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  }
+]);
